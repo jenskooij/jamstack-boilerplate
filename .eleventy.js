c143675663be2922f8ae717a8aad222e05b0c613
Jens Kooij <jenskooij1988@gmail.com>
2022-09-02 10:35:46 +0200

    Updated the regular template formats, added markdown-it as dependency

--- .eleventy.js
+++ .eleventy.js
@@ -4,19 +4,16 @@ const htmlmin = require("html-minifier"),
 module.exports = function (eleventyConfig) {
   eleventyConfig.setTemplateFormats([
     "njk",
-    "ico",
-    "png",
-    "xml"
+    "md"
   ]);
 
   eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
     if (outputPath.endsWith(".html")) {
-      let minified = htmlmin.minify(content, {
+      return htmlmin.minify(content, {
         useShortDoctype: true,
         removeComments: true,
         collapseWhitespace: true
       });
-      return minified;
     }
 
     return content;
